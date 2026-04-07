5061f0d711d51d5f1fc32126d3f7b253af3e75c2
Gourav Chouhan <143867453+Gourav0404@users.noreply.github.com>
2026-04-07 20:52:00 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -24,6 +24,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:5173",
+  "https://night-coding-marathon-3.onrender.com",
   "https://night-coding-marathon-eight.vercel.app/"
 ];
 
